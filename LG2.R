8a5b3d09a91159ae86595df43a1d476c28835409
LMDuarte <69866550+LMDuarte@users.noreply.github.com>
2020-10-07 15:49:27 -0500

    Update LG2.R

--- LG2.R
+++ LG2.R
@@ -1,19 +1,20 @@
+#separating responces by sexual orientation
 by(da37404.0001$SEXFRQ,da37404.0001$RTYP,summary)
-
+#simplify 
 sx=by(da37404.0001$SEXFRQ,da37404.0001$RTYP,summary)
 
 
-
+#turning frequency into percentage
 sx[["(1) Gay"]]
 proportions (sx[["(1) Gay"]])
 proportions (sx[["(2) Lesbian"]])
 proportions (sx[["(3) Straight"]])
 
-
+#simplifiying
 gsx= proportions (sx[["(1) Gay"]])
 
 print(gsx)
-
+#graphing
 barplot(gsx, ylim=c(0.00,0.30),main="sex frequency",names.arg=c("1",
                                              "2",
                                              "3",
